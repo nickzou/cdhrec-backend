a3f432354aa424b1acf1e4726f39c350fd94d0a0
Adrian <adriandz@spinmaster.com>
2020-11-08 17:33:17 -0500

    updating with fixes from phone call

--- sh/setup.sh
+++ sh/setup.sh
@@ -1,3 +1,3 @@
 composer install
 cd public
-wp plugin advanced-custom-fields-pro activate wp-graphiql wp-graphql wp-graphql-acf
+wp plugin activate advanced-custom-fields-pro wp-graphiql wp-graphql wp-graphql-acf acf-extended
